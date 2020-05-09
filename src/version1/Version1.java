9a9be8b7cfd1e9536bb14893198f91f0a937eb65
Luis Miguel Rios <luismrios1602@gmail.com>
2020-05-09 18:21:41 -0500

    mi primer commit

---
+++ src/version1/Version1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package version1;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Lenovo
+ */
+public class Version1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null, "Aprendiendo a usar git\n\nComandos:\nls: Listar todos tus directorios\npwd: Saber en que directorio te encuentras");
+    }
+    
+}
