d67c94108a4bbbcfa8389cefdcdb0b7da188d56b
Jesus Rios <jesusdrp09@gmail.com>
2020-05-09 20:22:24 -0500

    Ahora hay mas texto en el codigo

--- src/version1/Version1.java
+++ src/version1/Version1.java
@@ -16,7 +16,9 @@ public class Version1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       JOptionPane.showMessageDialog(null, "Aprendiendo a usar git\n\nComandos:\nls: Listar todos tus directorios\npwd: Saber en que directorio te encuentras");
+       JOptionPane.showMessageDialog(null, "Aprendiendo a usar git\n\nComandos:\nls: Listar todos tus directorios\npwd: Saber en que directorio te encuentras"+
+               "\ngit init: Para inicializar un proyecto nuevo\ngit status: Para ver el estado de nuestros archivos\ngit add (Nombre archivo): Para agregar un archivo al staging area"+
+               "\ngit");
     }
     
 }
